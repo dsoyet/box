7fbdad51d2c6000cdcd2e03bbb3ba40ecc0293c7
Gilbert Jeffrey <dsoyet@foxmail.com>
2024-10-27 20:50:40 +0800

    update init.ps1

--- packer_templates/scripts/windows/init.ps1
+++ packer_templates/scripts/windows/init.ps1
@@ -3,12 +3,16 @@ $ErrorActionPreference = 'Stop'
 # New-VMSwitch -Name "hyperv" -AllowManagementOS $True -NetAdapterName "Ethernet Instance 0"
 
 $edition = (Get-ComputerInfo).WindowsEditionId
+Write-Output "$edition"
 switch ($edition) {
     'ServerDatacenter' {
         Install-WindowsFeature Hyper-V -IncludeManagementTools -WarningAction SilentlyContinue
     }
+    'Enterprise' {
+        Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestart -WarningAction SilentlyContinue
+    }
     default {
-        Write-Output "$edition"
+        # Write-Output "$edition"
     }
 }
 

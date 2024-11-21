323e5a69a57db2f5159e1ba8eb42c45f04596287
Gilbert Jeffrey <dsoyet@foxmail.com>
2024-11-21 16:51:18 +0800

    update gentoo.pkrvars.hcl and gentoo.sh

--- os_pkrvars/bootstrap/gentoo.pkrvars.hcl
+++ os_pkrvars/bootstrap/gentoo.pkrvars.hcl
@@ -3,4 +3,4 @@ os_version              = "2024"
 os_arch                 = "x86_64"
 iso_urls                = ["/osx/Users/Share/Phone/gentoo.iso", ]
 iso_checksum            = "sha256:21813bdb67c9ac320984fbbafc7c34df7589ec688cb46a788e857e58f55feeee"
-boot_command            = ["<enter><wait5>43<enter><wait10><wait5>lsblk<enter>rc-service sshd start<enter>echo 'CREATE_MAIL_SPOOL=no'>>/etc/default/useradd<enter>useradd -m vagrant<enter>echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_vagrant;<enter>echo 'vagrant:vagrant'|chpasswd<enter>"]
+boot_command            = ["<enter><wait5>43<enter><wait10><wait10>lsblk<enter>rc-service sshd start<enter>echo 'CREATE_MAIL_SPOOL=no'>>/etc/default/useradd<enter>useradd -m vagrant<enter>echo 'vagrant ALL=(ALL) NOPASSWD:ALL' >/etc/sudoers.d/99_vagrant;<enter>echo 'vagrant:vagrant'|chpasswd<enter>"]
--- packer_templates/scripts/common/gentoo.sh
+++ packer_templates/scripts/common/gentoo.sh
@@ -1,19 +1,11 @@
 #!/bin/sh -eux
 
-CONFIG_SCRIPT='/usr/local/bin/arch-config.sh'
-if [ -b /dev/vda ]; then
-  ROOT_DISK='vda'
-  PKGS='base linux openssh sudo'
-fi
-if [ -b /dev/sda ]; then
-  ROOT_DISK='sda'
-  PKGS='base linux openssh sudo hyperv'
-fi
-
-ROOT_PARTITION="/dev/${ROOT_DISK}2"
+STAGE3_URL='https://mirrors.nju.edu.cn/gentoo/releases/amd64/autobuilds/current-stage3-amd64-desktop-systemd/stage3-amd64-desktop-systemd-20241117T163407Z.tar.xz'
+CONFIG_SCRIPT='/usr/local/bin/gentoo-config.sh'
+ROOT_DISK=$(ls /dev/[sv]da)
 
 # Partition and format disk
-parted "/dev/${ROOT_DISK}" ---pretend-input-tty <<EOF
+parted "${ROOT_DISK}" ---pretend-input-tty <<EOF
 mktable gpt
 mkpart ESP fat32 0% 5%
 mkpart primary 5% 95%
@@ -22,66 +14,71 @@ set 1 esp on
 quit
 EOF
 
-mkfs.btrfs ${ROOT_PARTITION} 
-mkfs.fat -F 32 /dev/${ROOT_DISK}1
-mount ${ROOT_PARTITION} /mnt
-mkdir /mnt/efi
-mount /dev/${ROOT_DISK}1 /mnt/efi
+mkfs.btrfs ${ROOT_DISK}2 && mkfs.fat -F 32 ${ROOT_DISK}1
+mount --mkdir ${ROOT_DISK}2 /mnt/gentoo
+mount --mkdir ${ROOT_DISK}1 /mnt/gentoo/efi
 
-wget -q https://mirrors.nju.edu.cn/gentoo/releases/amd64/autobuilds/current-stage3-amd64-desktop-systemd/stage3-amd64-desktop-systemd-20241117T163407Z.tar.xz
-tar xpf stage3-*.tar.xz --xattrs-include='*.*' --numeric-owner -C /mnt
+curl -SsLl ${STAGE3_URL} | tar xJp -C /mnt/gentoo --xattrs --numeric-owner
 
-mount --bind /run /mnt/run && mount --make-slave /mnt/run
-mount --types proc /proc /mnt/proc
-mount --rbind /sys /mnt/sys && mount --make-rslave /mnt/sys
-mount --rbind /dev /mnt/dev && mount --make-rslave /mnt/dev
+mount --rbind /sys /mnt/gentoo/sys && mount --make-rslave /mnt/gentoo/sys
+mount --types proc /proc /mnt/gentoo/proc
+mount --bind /run /mnt/gentoo/run && mount --make-slave /mnt/gentoo/run
+mount --rbind /dev /mnt/gentoo/dev && mount --make-rslave /mnt/gentoo/dev
+cp /etc/resolv.conf /mnt/gentoo/etc/resolv.conf
 
-# Installation
-cat << 'EOF' > /mnt/etc/portage/binrepos.conf/gentoobinhost.conf
-[gentoobinhost]
-priority = 1
-sync-uri = https://mirrors.nju.edu.cn/gentoo/releases/amd64/binpackages/23.0/x86-64
-EOF
-cat << 'EOF' > /mnt/etc/locale.gen
-en_US.UTF-8 UTF-8
-EOF
-cat << 'EOF' > /mnt/etc/resolv.conf
-nameserver 223.5.5.5
-EOF
-cat << 'EOF' > /mnt/etc/portage/make.conf
-COMMON_FLAGS="-O2 -pipe"
-CFLAGS="${COMMON_FLAGS}"
-CXXFLAGS="${COMMON_FLAGS}"
-FCFLAGS="${COMMON_FLAGS}"
-FFLAGS="${COMMON_FLAGS}"
-
-ACCEPT_LICENSE="-* @FREE @BINARY-REDISTRIBUTABLE"
-FEATURES="${FEATURES} getbinpkg"
-FEATURES="${FEATURES} binpkg-request-signature"
-
-LC_MESSAGES=C.utf8
+cat << 'EOF' > /mnt/gentoo/etc/portage/make.conf
+EMERGE_DEFAULT_OPTS="--quiet-build --jobs=4 --load-average=4 --autounmask-continue"
+ACCEPT_LICENSE="*"
 GENTOO_MIRRORS="http://mirrors.nju.edu.cn/gentoo/"
 EOF
-cat << 'EOF' > /mnt/etc/portage/package.accept_keywords/installkernel
-sys-kernel/installkernel
-sys-boot/uefi-mkconfig
-app-emulation/virt-firmware
+cat << 'EOF' > /mnt/gentoo/etc/locale.gen
+en_GB ISO-8859-1
+en_GB.UTF-8 UTF-8
 EOF
-cat << 'EOF' > /mnt/etc/portage/package.use/installkernel
-sys-kernel/installkernel efistub
+cat << EOF > /mnt/gentoo/etc/kernel/cmdline
+root=${ROOT_DISK}2
 EOF
-cat << 'EOF' > /mnt/etc/kernel/cmdline
-root=/dev/vda2
+cat > /mnt/gentoo/etc/fstab <<EOF
+${ROOT_DISK}1 /efi  vfat noauto,noatime    1 2
+${ROOT_DISK}3 none  swap sw                0 0
+${ROOT_DISK}2 /     ext4 noauto,noatime    0 1
 EOF
-cat << 'EOF' > /mnt/etc/portage/package.use/uki
-sys-apps/systemd boot ukify
-sys-kernel/installkernel dracut ukify uki
+mkdir -p /mnt/gentoo/etc/portage/repos.conf
+cat << 'EOF' > /mnt/gentoo/etc/portage/repos.conf/gentoo.conf
+[DEFAULT]
+main-repo = gentoo
+[gentoo]
+location = /var/db/repos/gentoo
+sync-type = rsync
+sync-uri = rsync://rsync.mirrors.ustc.edu.cn/gentoo-portage
+auto-sync = yes
+sync-rsync-verify-jobs = 1
+sync-rsync-verify-metamanifest = yes
+sync-rsync-verify-max-age = 3
+sync-openpgp-key-path = /usr/share/openpgp-keys/gentoo-release.asc
+sync-openpgp-keyserver = hkps://keys.gentoo.org
+sync-openpgp-key-refresh-retry-count = 40
+sync-openpgp-key-refresh-retry-overall-timeout = 1200
+sync-openpgp-key-refresh-retry-delay-exp-base = 2
+sync-openpgp-key-refresh-retry-delay-max = 60
+sync-openpgp-key-refresh-retry-delay-mult = 4
+sync-webrsync-verify-signature = yes
 EOF
 
 echo sleep...
 sleep 3600
 
 # emerge-webrsync
-# emerge --ask sys-kernel/installkernel sys-kernel/linux-firmware sys-firmware/intel-microcode
+#equery u sys-apps/systemd
+# emerge --ask sys-kernel/gentoo-sources app-portage/gentoolkit
+# eselect kernel set 1
+# emerge --ask sys-kernel/linux-firmware sys-firmware/intel-microcode 
+# emerge --ask sys-kernel/installkernel
+# emerge --ask sys-kernel/gentoo-kernel
 # locale-gen
 # ln -sf ../usr/share/zoneinfo/Europe/Brussels /etc/localtime
+# umount -R /mnt/gentoo
+# default/linux/amd64/23.0/desktop/systemd
+
+# libbpf: failed to find '.BTF' ELF section in vmlinux
+# FAILED: load BTF from vmlinux: No data available
